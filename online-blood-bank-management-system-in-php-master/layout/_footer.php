b996aea852880cb2d298a9b3f1e8c749aa6773bb
Soniya P Malode <71498541+soniya-malode@users.noreply.github.com>
2020-12-27 14:35:40 +0530

    Add files via upload

---
+++ online-blood-bank-management-system-in-php-master/layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="foter">
+            <b> <a href="http://abcd.in">abcde</a></b>
+            <br>
+            &COPY;
+</footer>
+
+
+
+</body>
+</html>
---
+++ online-blood-bank-management-system-in-php-master/users/layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="nav navbar-fixed-bottom">
+            <b> <a href="">abcde</a></b>
+            <br>
+            &COPY;
+</footer>
+
+
+
+</body>
+</html>
